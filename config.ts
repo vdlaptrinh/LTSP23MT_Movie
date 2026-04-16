f2fb65ba73a3312b95435fd77558f8f3d5f2aac7
VanDai <vdlaptrinh@gmail.com>
2026-04-16 14:50:11 +0700

    remove config.ts

--- config.ts
+++
@@ -1,3 +0,0 @@
-export const TMDB_API_KEY = "a481ee5f9ae9a16d313721a2f63a9e69";
-export const TMDB_API_URL = "https://api.themoviedb.org/3";
-export const TMDB_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
